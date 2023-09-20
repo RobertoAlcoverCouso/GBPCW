7fd6029bd5e4618ebfddf7be2b25b6cce8fcd112
RobertoAlcoverCouso <robertoalcovercouso@gmail.com>
2023-09-20 15:12:16 +0200

    first commit

---
+++ mmseg/models/utils/dacs_transforms.py
@@ -0,0 +1,156 @@
+# Obtained from: https://github.com/vikolss/DACS
+# Copyright (c) 2020 vikolss. Licensed under the MIT License
+# A copy of the license is available at resources/license_dacs
+
+import kornia
+import numpy as np
+import torch
+import torch.nn as nn
+
+import torch
+import torch.nn as nn
+
+
+class MMD_loss(nn.Module):
+    def __init__(self, kernel_mul = 2.0, kernel_num = 5):
+        super(MMD_loss, self).__init__()
+        self.kernel_num = kernel_num
+        self.kernel_mul = kernel_mul
+        self.fix_sigma = None
+        return
+    def guassian_kernel(self, source, target, kernel_mul=2.0, kernel_num=5, fix_sigma=None):
+        n_samples = int(source.size()[0])+int(target.size()[0])
+        total = torch.cat([source, target], dim=0)
+
+        total0 = total.unsqueeze(0).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+        total1 = total.unsqueeze(1).expand(int(total.size(0)), int(total.size(0)), int(total.size(1)))
+        L2_distance = ((total0-total1)**2).sum(2) 
+        if fix_sigma:
+            bandwidth = fix_sigma
+        else:
+            bandwidth = torch.sum(L2_distance.data) / (n_samples**2-n_samples)
+        bandwidth /= kernel_mul ** (kernel_num // 2)
+        bandwidth_list = [bandwidth * (kernel_mul**i) for i in range(kernel_num)]
+        kernel_val = [torch.exp(-L2_distance / bandwidth_temp) for bandwidth_temp in bandwidth_list]
+        return sum(kernel_val)
+
+    def forward(self, source, target):
+        batch_size = int(source.size()[0])
+        kernels = guassian_kernel(source, target, kernel_mul=self.kernel_mul, kernel_num=self.kernel_num, fix_sigma=self.fix_sigma)
+        XX = kernels[:batch_size, :batch_size]
+        YY = kernels[batch_size:, batch_size:]
+        XY = kernels[:batch_size, batch_size:]
+        YX = kernels[batch_size:, :batch_size]
+        loss = torch.mean(XX + YY - XY -YX)
+        return loss
+
+def strong_transform(param, data=None, target=None):
+    assert ((data is not None) or (target is not None))
+    data, target = one_mix(mask=param['mix'], data=data, target=target)
+    data, target = color_jitter(
+        color_jitter=param['color_jitter'],
+        s=param['color_jitter_s'],
+        p=param['color_jitter_p'],
+        mean=param['mean'],
+        std=param['std'],
+        data=data,
+        target=target)
+    data, target = gaussian_blur(blur=param['blur'], data=data, target=target)
+    return data, target
+
+
+def get_mean_std(img_metas, dev):
+    mean = [
+        torch.as_tensor(img_metas[i]['img_norm_cfg']['mean'], device=dev)
+        for i in range(len(img_metas))
+    ]
+    mean = torch.stack(mean).view(-1, 3, 1, 1)
+    std = [
+        torch.as_tensor(img_metas[i]['img_norm_cfg']['std'], device=dev)
+        for i in range(len(img_metas))
+    ]
+    std = torch.stack(std).view(-1, 3, 1, 1)
+    return mean, std
+
+
+def denorm(img, mean, std):
+    return img.mul(std).add(mean) / 255.0
+
+
+def denorm_(img, mean, std):
+    img.mul_(std).add_(mean).div_(255.0)
+
+
+def renorm_(img, mean, std):
+    img.mul_(255.0).sub_(mean).div_(std)
+
+
+def color_jitter(color_jitter, mean, std, data=None, target=None, s=.25, p=.2):
+    # s is the strength of colorjitter
+    if not (data is None):
+        if data.shape[1] == 3:
+            if color_jitter > p:
+                if isinstance(s, dict):
+                    seq = nn.Sequential(kornia.augmentation.ColorJitter(**s))
+                else:
+                    seq = nn.Sequential(
+                        kornia.augmentation.ColorJitter(
+                            brightness=s, contrast=s, saturation=s, hue=s))
+                denorm_(data, mean, std)
+                data = seq(data)
+                renorm_(data, mean, std)
+    return data, target
+
+
+def gaussian_blur(blur, data=None, target=None):
+    if not (data is None):
+        if data.shape[1] == 3:
+            if blur > 0.5:
+                sigma = np.random.uniform(0.15, 1.15)
+                kernel_size_y = int(
+                    np.floor(
+                        np.ceil(0.1 * data.shape[2]) - 0.5 +
+                        np.ceil(0.1 * data.shape[2]) % 2))
+                kernel_size_x = int(
+                    np.floor(
+                        np.ceil(0.1 * data.shape[3]) - 0.5 +
+                        np.ceil(0.1 * data.shape[3]) % 2))
+                kernel_size = (kernel_size_y, kernel_size_x)
+                seq = nn.Sequential(
+                    kornia.filters.GaussianBlur2d(
+                        kernel_size=kernel_size, sigma=(sigma, sigma)))
+                data = seq(data)
+    return data, target
+
+
+def get_class_masks(labels):
+    class_masks = []
+    for label in labels:
+        classes = torch.unique(labels)
+        nclasses = classes.shape[0]
+        class_choice = np.random.choice(
+            nclasses, int((nclasses + nclasses % 2) / 2), replace=False)
+        classes = classes[torch.Tensor(class_choice).long()]
+        class_masks.append(generate_class_mask(label, classes).unsqueeze(0))
+    return class_masks
+
+
+def generate_class_mask(label, classes):
+    label, classes = torch.broadcast_tensors(label,
+                                             classes.unsqueeze(1).unsqueeze(2))
+    class_mask = label.eq(classes).sum(0, keepdims=True)
+    return class_mask
+
+
+def one_mix(mask, data=None, target=None):
+    if mask is None:
+        return data, target
+    if not (data is None):
+        stackedMask0, _ = torch.broadcast_tensors(mask[0], data[0])
+        data = (stackedMask0 * data[0] +
+                (1 - stackedMask0) * data[1]).unsqueeze(0)
+    if not (target is None):
+        stackedMask0, _ = torch.broadcast_tensors(mask[0], target[0])
+        target = (stackedMask0 * target[0] +
+                  (1 - stackedMask0) * target[1]).unsqueeze(0)
+    return data, target
